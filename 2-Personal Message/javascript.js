c7359b7d7092221656fb76efe76587db2e5ccc59
shahbaz-j <114741603+shahbaz-j@users.noreply.github.com>
2022-10-03 17:33:23 +0500

    Add files via upload

---
+++ 2-Personal Message/javascript.js
@@ -0,0 +1,3 @@
+
+var person_name = "Malik Shahbaz Khan";
+document.getElementById("name").innerHTML = "Hello" + " " + person_name + "would you like to learn some JavaScript today?";
---
+++ 3-Name Cases/javascript.js
@@ -0,0 +1,8 @@
+
+var lowercase = "Malik Shahbaz Khan"; 
+document.getElementById("lc").innerHTML = lowercase.toLowerCase();
+
+var uppercase = "Malik Shahbaz Khan"; 
+document.getElementById("uc").innerHTML = uppercase.toUpperCase();
+
+
